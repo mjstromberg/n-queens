c1987eac131b1d9d9a2eee220fe6ddb4d941ded3
mjstromberg <michaeljstromberg@gmail.com>
2016-09-21 18:11:46 -0700

    Implement countNQueensSolutions in solvers.js

--- src/solvers.js
+++ src/solvers.js
@@ -66,15 +66,76 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution;
+  var solutionFound = false;
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+  var recurseQueen = function(levelsLeft, boardRows) {
+    //for every starting on the top row
+    boardRows = boardRows || {n: n};
+    
+    var thisBoard = new Board(boardRows);
+    
+    for (var i = 0; i < n; i++) {
+      if (!solutionFound) {
+        thisBoard.togglePiece(levelsLeft, i);
+        //create a board with that starting point
+        if (!thisBoard.hasAnyQueenConflictsOn(levelsLeft, i)) {
+          if (levelsLeft === 0) {
+            solution = new Board(thisBoard.rows()).rows();
+            console.log(solution[0] === thisBoard.rows()[0]);
+            return;
+          } else {
+            recurseQueen(levelsLeft - 1, thisBoard.rows());
+          }
+        }
+        
+        thisBoard.togglePiece(levelsLeft, i);
+      }
+    }
+  };
+    
+  recurseQueen(n - 1);
+  console.log(n);
+  console.log(solution);
+  return n === 0 ? new Board({n: n}).rows() : solution;
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+
+  if (n === 0) {
+    return 1;
+  }
+
+  var recurseQueen = function(levelsLeft, boardRows) {
+    //for every starting on the top row
+    boardRows = boardRows || {n: n};
+    
+    var thisBoard = new Board(boardRows);
+    
+    for (var i = 0; i < n; i++) {
+      thisBoard.togglePiece(levelsLeft, i);
+      //create a board with that starting point
+      if (!thisBoard.hasAnyQueenConflictsOn(levelsLeft, i)) {
+        if (levelsLeft === 0) {
+          solutionCount++;
+        } else {
+          recurseQueen(levelsLeft - 1, thisBoard.rows());
+        }
+      }
+      
+      thisBoard.togglePiece(levelsLeft, i);
+    }
+    
+  };
+    
+  recurseQueen(n - 1);
+  
+   
+    //create a board for each starting point below that
+      //recurse as necessary
+        //if n >= n solution count ++
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
